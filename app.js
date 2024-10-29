d0a35a6c7a3ed8abfaef312d4b393486590ab484
Erlend <erlendk.kodehode@gmail.com>
2024-10-29 12:58:16 +0100

    done

--- app.js
+++ app.js
@@ -23,12 +23,30 @@ the names of all products in a specific category (e.g. 'electronics')
 ******************************************************************************/
 
 const products = [
-    { name: "Smartphone", price: 800, category: "electronics" },
-    { name: "Headphones", price: 150, category: "electronics" },
-    { name: "Coffee Maker", price: 100, category: "home appliances" },
-    { name: "Blender", price: 250, category: "home appliances" },
-    { name: "Laptop", price: 1200, category: "electronics" },
-    { name: "T-shirt", price: 50, category: "clothing" },
-    { name: "Sneakers", price: 300, category: "clothing" }
-  ];
+  { name: "Smartphone", price: 800, category: "electronics" },
+  { name: "Headphones", price: 150, category: "electronics" },
+  { name: "Coffee Maker", price: 100, category: "home appliances" },
+  { name: "Blender", price: 250, category: "home appliances" },
+  { name: "Laptop", price: 1200, category: "electronics" },
+  { name: "T-shirt", price: 50, category: "clothing" },
+  { name: "Sneakers", price: 300, category: "clothing" },
+];
 
+//makes a list of products that costs less than 200 by filtering out any that costs above that
+const cheapProducts = products.filter((e) => e.price < 200);
+
+//makes a list that only contains the product names
+const productNames = products.map((e) => e.name);
+
+//variable to change what you are filtering for
+const filterCateg = "electronics";
+//filters out products out of a specific category and puts the names of the products in a list
+const specifProductNames = products
+  .filter((e) => e.category === filterCateg)
+  .map((e) => e.name);
+
+//a bool that is true if there are any products that costs more than 1000
+expensiveProducts = products.some((e) => e.price > 1000);
+
+//the total cost of the products
+totalCost = products.reduce((total, e) => total + e.price, 0);
